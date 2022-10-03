6b16437f8dfbb4ce484a9edd4ee8a0ae2c7bec1a
Matthew Stanciu <mattbstanciu@gmail.com>
2022-10-03 03:32:12 -0400

    Add extra validation

--- lib/validate.ts
+++ lib/validate.ts
@@ -1,6 +1,7 @@
 const validate = (lightningTime: string) => {
   const tildeCount = (lightningTime.match(/~/g) || []).length;
-  return tildeCount === 2 && lightningTime.length === 5;
+  const validLetters = /[a-f]+$/g.test(lightningTime.replace("~", ""));
+  return tildeCount === 2 && lightningTime.length === 5 && validLetters;
 };
 
 export default validate;
